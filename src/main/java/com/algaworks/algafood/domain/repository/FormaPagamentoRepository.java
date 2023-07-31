6e1d4fd0e6b1b9f6bc690f6f6de1669bd9e6d428
Giovanne Mendonca <giovannemendonca@outlook.com>
2023-07-31 20:02:21 -0300

    project

---
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,2 @@
+package com.algaworks.algafood.domain.repository;public interface FormaPagamentoRepository {
+}
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,2 @@
+package com.algaworks.algafood.infrastructure.repository;public class CozinhaRepositoryImpl {
+}
