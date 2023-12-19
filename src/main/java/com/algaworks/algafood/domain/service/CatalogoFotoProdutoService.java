3bee76cfce734cf0cc40498b4f493548b33d18a6
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-12-19 20:56:48 -0300

    Excluindo e substituindo cadastro de foto de produto

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -5,17 +5,31 @@ import com.algaworks.algafood.domain.repository.ProdutoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CatalogoFotoProdutoService {
 
-    private ProdutoRepository produtoRepository;
+    private final ProdutoRepository produtoRepository;
 
-    CatalogoFotoProdutoService(ProdutoRepository produtoRepository){
+    CatalogoFotoProdutoService(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
     }
 
     @Transactional
-    public FotoProduto salvar(FotoProduto fotoProduto){
-        return produtoRepository.save(fotoProduto);
+    public FotoProduto salvar(FotoProduto foto) {
+
+
+        Long restauranteId = foto.getRestauranteId();
+        Long produtoId = foto.getProduto().getId();
+
+        var fotoExistente = produtoRepository
+                .findFotoById(restauranteId, produtoId);
+
+        if (fotoExistente.isPresent()) {
+            produtoRepository.delete(fotoExistente.get());
+        }
+
+        return produtoRepository.save(foto);
     }
 }
