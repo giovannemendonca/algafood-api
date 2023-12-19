f08346d6f169bb90adade690a25e060bde56502f
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-12-19 18:37:02 -0300

    Validando o content type do arquivo

--- src/main/java/com/algaworks/algafood/api/model/dto/input/FotoProdutoInputDTO.java
+++ src/main/java/com/algaworks/algafood/api/model/dto/input/FotoProdutoInputDTO.java
@@ -1,10 +1,12 @@
 package com.algaworks.algafood.api.model.dto.input;
 
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
@@ -12,7 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class FotoProdutoInputDTO {
 
     @NotNull
-    @FileSize(max="500KB")
+    @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile arquivo;
 
     @NotBlank
