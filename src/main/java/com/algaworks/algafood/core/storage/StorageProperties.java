b5115741b3f8ee07a4d7bdcfee0c8b4c3a6eb1ca
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-12-21 18:12:59 -0300

    Definindo bean do client da Amazon S3 e configurando credenciais

--- src/main/java/com/algaworks/algafood/core/storage/StorageProperties.java
+++ src/main/java/com/algaworks/algafood/core/storage/StorageProperties.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.storage;
 
+import com.amazonaws.regions.Regions;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -32,7 +33,7 @@ public class StorageProperties {
         private String idChaveAcesso;
         private String chaveAcessoSecreta;
         private String bucket;
-        private String regiao;
+        private Regions regiao;
         private String diretorioFotos;
         private String catalogo;
     }
