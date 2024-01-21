e81f49d0044ed2bf10f32c3bdff4daee2805f778
Giovanne Mendonça <giovannemendonca@outlook.com>
2024-01-21 12:21:43 -0300

    Depreciando uma versão da API

--- src/main/java/com/algaworks/algafood/api/v1/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/CidadeController.java
@@ -37,6 +37,7 @@ public class CidadeController {
     @Autowired
     private CidadeInputDTODisassembler cidadeInputDTODisassembler;
 
+    @Deprecated
     @GetMapping
     public CollectionModel<CidadeDTO> listar() {
 
--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -2,15 +2,21 @@ package com.algaworks.algafood.core.web;
 
 
 import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiDeprecationHandler apiDeprecationHandler;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -20,6 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE"); // todos os métodos
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiDeprecationHandler);
+    }
 
     // esse bean é necessário para que o Spring possa interceptar as requisições e adicionar o cabeçalho ETag
     @Bean
